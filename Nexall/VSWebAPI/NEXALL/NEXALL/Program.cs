75c821037c6863807cd5c2bedb8cc80d81c2061b
JV-33 <virsnitisjanis@icloud.com>
2023-10-31 16:37:07 +0200

    FE

--- Nexall/VSWebAPI/NEXALL/NEXALL/Program.cs
+++ Nexall/VSWebAPI/NEXALL/NEXALL/Program.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Nexall.Data;
 using Nexall.Data.DataContext;
 using Nexall.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace NEXALL
 {
@@ -14,35 +18,36 @@ namespace NEXALL
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Pievienot datu kontekstu un interfeisu
             builder.Services.AddDbContext<NexallContext>(options =>
                 options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+            builder.Services.AddScoped<INexallContext, NexallContext>();
 
+            // Pievienot servisus
             builder.Services.AddScoped<ICarStatisticsService, CarStatisticsService>();
+            builder.Services.AddScoped<DataImportService>();
 
             builder.Services.AddCors(options =>
             {
-                options.AddDefaultPolicy(builder =>
+                options.AddDefaultPolicy(policyBuilder =>
                 {
-                    builder.WithOrigins("http://localhost:3000")
+                    policyBuilder.WithOrigins("http://localhost:3000")
                           .AllowAnyHeader()
-                           .AllowAnyMethod();
+                          .AllowAnyMethod();
                 });
             });
 
-            builder.Services.AddScoped<DataImportService>();
-
             var app = builder.Build();
 
-            //Ielādēt datus no speed.txt faila
+            // Ielādēt datus no speed.txt faila
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
                 try
                 {
-                    var context = services.GetRequiredService<NexallContext>();
+                    var context = services.GetRequiredService<INexallContext>();
                     var dataImportService = services.GetRequiredService<DataImportService>();
                     dataImportService.ImportData("../Nexall.Data/speed.txt");
-
                 }
                 catch (Exception ex)
                 {
@@ -58,11 +63,8 @@ namespace NEXALL
             }
 
             app.UseHttpsRedirection();
-
             app.UseCors();
-
             app.UseAuthorization();
-
             app.MapControllers();
             app.Run();
         }
